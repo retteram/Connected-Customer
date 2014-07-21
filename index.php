72e59b527ab6dca284fd436dd070e22d37479c7d
retteram <retteramj@gmail.com>
2014-07-21 17:24:11 -0400

    Hopefully fixed errors

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Redirect browser */
-header("Location: htdocs/pages/connected-customer_main-page.php");
- 
-/* Make sure that code below does not get executed when we redirect. */
-exit;
-?>
