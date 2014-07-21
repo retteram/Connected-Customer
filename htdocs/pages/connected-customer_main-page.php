053f0858186c055561bbc8a6dc3a699d0a333ada
retteram <retteramj@gmail.com>
2014-07-21 11:33:25 -0400

    FIXED back to normal with the gradient change

--- htdocs/pages/connected-customer_main-page.php
+++ htdocs/pages/connected-customer_main-page.php
@@ -18,7 +18,47 @@
 				<div class="title-bar">
 					<div class="title-bar-wrapper">
 						<div id="placeholder"></div>
-						<h1 id="title">WHY ARE YOU HERE?</h1>
+						<h1 id="title">HOW CAN WE HELP YOU?</h1>
+						<span class="menu-bars" onclick="menu_button()">
+							<div class="white-line"></div>
+							<div class="white-line"></div>
+							<div class="white-line"></div>
+							<!-- <img src="../assets/menu-bars-2.png" width="100%"/> -->
+						</span>
+					</div>
+				</div>
+				<div class="nav-dropdown">
+					<ul>
+						<li>TRANSACTION&nbsp</li>
+						<li>MEET WITH A BANKER&nbsp</li>
+						<li>ACCOUNT INFO&nbsp</li>
+						<li>LOAN INFO&nbsp</li>
+						<li>CREDIT CARD&nbsp</li>
+						<li>PROBLEM RESOLUTION&nbsp</li>
+						<li>FINANCIAL ADVICE&nbsp</li>
+						<li>FINANCIAL HOW-TO'S&nbsp</li>
+						<li>SAFETY DEPOSIT BOX&nbsp</li>
+						<li>LOCAL EVENT&nbsp</li>
+						<li>PLAY A GAME&nbsp</li>
+						<li>FUN FACT&nbsp</li>
+					</ul>
+				</div>
+			</div>
+			<!-- END Header -->
+
+
+			<!-- BEGIN Main Content -->
+			<?php include("carousel.php"); ?>
+			<!-- END Main Content -->
+
+
+			<!-- BEGIN Footer -->
+			<div id="footer">
+				<div id="header">
+				<div class="title-bar">
+					<div class="title-bar-wrapper">
+						<div id="placeholder"></div>
+						<h1 id="title">HOW CAN WE HELP YOU?</h1>
 						<span class="menu-bars" onclick="menu_button()">
 							<div class="white-line"></div>
 							<div class="white-line"></div>
@@ -36,16 +76,8 @@
 					</ul>
 				</div>
 			</div>
-			<!-- END Header -->
 
-
-			<!-- BEGIN Main Content -->
-			<?php include("carousel.php"); ?>
-			<!-- END Main Content -->
-
-
-			<!-- BEGIN Footer -->
-			<div id="footer"></div>
+			</div>
 			<!-- END Footer -->
 		</div>
 	</body>
