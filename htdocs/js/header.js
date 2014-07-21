22f5af10c27066ca9ed62f098bf805c1bbf6b3f9
retteram <retteramj@gmail.com>
2014-07-21 15:57:58 -0400

    header

--- htdocs/js/header.js
+++ htdocs/js/header.js
@@ -31,13 +31,13 @@ function set_clock() {
 	var daynum = currentTime.getDay();
 	var day = "";
 	switch(daynum){
-		case 0: day = "sunday"; break;
-		case 1: day = "pm monday"; break;
-		case 2: day = "tuesday"; break;
-		case 3: day = "wednesday"; break;
-		case 4: day = "thursday"; break;
-		case 5: day = "friday"; break;
-		case 6: day = "saturday"; break;
+		case 0: day = "Sunday"; break;
+		case 1: day = "Monday"; break;
+		case 2: day = "Tuesday"; break;
+		case 3: day = "Wednesday"; break;
+		case 4: day = "Thursday"; break;
+		case 5: day = "Friday"; break;
+		case 6: day = "Saturday"; break;
 	}
 
 	$(".dayofweek").text(day);
