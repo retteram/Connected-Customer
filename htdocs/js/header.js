c5a8b2b90d9ffa6cd003db64f6b65553de39c274
retteram <retteramj@gmail.com>
2014-07-21 16:43:14 -0400

    banking-101

--- htdocs/js/header.js
+++ htdocs/js/header.js
@@ -34,7 +34,7 @@ function set_clock() {
 	var daynum = currentTime.getDay();
 	var day = "";
 	switch(daynum){
-<<<<<<< HEAD
+
 		case 0: day = "Sunday"; break;
 		case 1: day = "Monday"; break;
 		case 2: day = "Tuesday"; break;
@@ -42,15 +42,7 @@ function set_clock() {
 		case 4: day = "Thursday"; break;
 		case 5: day = "Friday"; break;
 		case 6: day = "Saturday"; break;
-=======
-		case 0: day = "sunday"; break;
-		case 1: day = "monday"; break;
-		case 2: day = "tuesday"; break;
-		case 3: day = "wednesday"; break;
-		case 4: day = "thursday"; break;
-		case 5: day = "friday"; break;
-		case 6: day = "saturday"; break;
->>>>>>> origin/master
+
 	}
 
 	$(".dayofweek").text(day);
