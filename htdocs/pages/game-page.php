9b45d26d0049a31f6586ca2b4abdcabd60aefedf
IsaacVH <isaac.vanh@gmail.com>
2014-07-23 10:56:13 -0400

    Game page changes and html link updates.

--- htdocs/pages/game-page.php
+++ htdocs/pages/game-page.php
@@ -4,6 +4,7 @@
 		<title>Game</title>
 		<?php require("page-pieces/imports.php"); ?>
 		<script type="text/javascript" src="../js/game-page.js"></script>
+		<link href="../css/howTo.css" rel="stylesheet">
 		<script>var backlink = "connected-customer_main-page.php";</script>
 	</head>
 	<body>
@@ -11,14 +12,37 @@
 
 			<!-- BEGIN Header -->
 			<?php require("page-pieces/header.php"); ?>
+			<div class="headBox2">
+			<h1 id="title">GAMES</h1>
+			</div>
 			<!-- END Header -->
 
 			<!-- BEGIN Main Content -->
-			<?php require("page-pieces/carousel-sub-Page.php"); ?>
+			<div class="game-container">
+			<img src="../assets/gameScreen.png" />
+			</div>
 			<!-- END Main Content -->
 
 			<!-- BEGIN Footer -->
+			<div class="related-container">
+				<div class="related-object">
+					<h1 id="relatedTitle">MORE GAMES</h1>
+					<div class="object1">
+					<img src="../assets/related1.png" />
+					<h2 id="objectTitle">TIC TAC TOE</h2>
+					</div>
+					<div class="object2">
+					<img src="../assets/related2.png" />
+					<h2 id="objectTitle">FINANCIAL MOUNTAIN CLIMBING</h2>	
+					</div>
+					<div class="object3">
+					<img src="../assets/related3.png" />
+					<h2 id="objectTitle">PIG BANK EXPLOSION</h2>
+					</div>
+				</div>
+			</div>
 			<?php require("page-pieces/footer-simple.php"); ?>
+			<!-- END Footer -->
 			<!-- END Footer -->
 		</div>
 	</body>
