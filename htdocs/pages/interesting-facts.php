20ecd1b683376a3cda337a30756fc1067ba45669
IsaacVH <isaac.vanh@gmail.com>
2014-07-24 16:08:29 -0400

    Other file addition.

---
+++ htdocs/pages/interesting-facts.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Interesting Facts</title>
+		<?php require("page-pieces/imports.php"); ?>
+		<script src="../js/interesting-facts.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+		<?php require("page-pieces/header.php"); ?>
+
+		<?php require("page-pieces/vertical-list.php"); ?>
+
+		<?php require("page-pieces/footer-simple.php"); ?>
+	</body>
+</html>
