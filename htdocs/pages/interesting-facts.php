48f5ef8dc84892cc823d808d6e7d237ae7abacbb
retteram <retteramj@gmail.com>
2014-07-28 12:17:45 -0400

    fact styling undone

--- htdocs/pages/interesting-facts.php
+++ htdocs/pages/interesting-facts.php
@@ -4,13 +4,19 @@
 		<title>Interesting Facts</title>
 		<?php require("page-pieces/imports.php"); ?>
 		<script src="../js/interesting-facts.js" type="text/javascript"></script>
+		<link rel="stylesheet" type="text/css" href="../css/interesting-joking.css" />
 	</head>
 
 	<body>
 		<div id="home" data-role="page">
 			<?php require("page-pieces/header.php"); ?>
+			<div class="headBox1">
+			<h1 id="title">INTERESTING FACTS</h1>
+			</div>
 
+			
 			<?php require("page-pieces/display-fact.php"); ?>
+
 
 			<?php require("page-pieces/footer-simple.php"); ?>
 		</div>
--- htdocs/pages/page-pieces/display-fact.php
+++ htdocs/pages/page-pieces/display-fact.php
@@ -1,27 +1,58 @@
 <style>
+#factHolder{
+}
+
 .fact{
 	display:none;
 }
 
-.showFact{
-	display:block;
+
+.did-you-know{
+	
 }
+.showFact{
+	padding:20px;
+	display:block;
+	height:400px;
+	background-color:blue;
+	color:white;
+	font-family:helvetica;
+	font-weight: 900;
+	color:#37a85c;
+	font-size:50px;
+}
+
+.fact-container{
+	
+}
+
+.display-area{
+	
+}
+
 </style>
 
 <script type="text/javascript" src="../js/display-fact.js"></script>
 
-<div id="factsHolder">
-	<?php
-		$json = file_get_contents("../json/facts.json");
-		$facts = json_decode($json, true);
+
+<div class="fact-container">
+
+	<div class="did-you-know">
+	<div class="display-area">
+	<div id="factsHolder">
+		<?php
+			$json = file_get_contents("../json/facts.json");
+			$facts = json_decode($json, true);
 
 		
-		foreach ($facts as $value) {
-			echo "<div class='fact'>$value</div>";
-		}
-		// echo $facts[$val];
-	?>
-
+			foreach ($facts as $value) {
+				echo "<div class='fact'>$value</div>";
+			}
+			// echo $facts[$val];
+		?>
+	</div>
+	</div>
+	</div>
 
 </div>
 <button id="random" onclick="generateRand()">Generate Random Fact</button>
