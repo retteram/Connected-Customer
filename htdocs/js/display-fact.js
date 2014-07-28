d8398231546a1e5f6de208d84a8847539c3815cc
IsaacVH <isaac.vanh@gmail.com>
2014-07-28 12:01:56 -0400

    Fixed fact generator; no repeat until all shown

--- htdocs/js/display-fact.js
+++ htdocs/js/display-fact.js
@@ -1,13 +1,40 @@
-function generateRandomFact(first, last) {
-		return Math.floor(Math.random() * (last - first + 1)) + first;
-	}
+// This function just generates a number between 'first' and 'last'
+function generateRandomNumber(first, last) {
+	return Math.floor(Math.random() * (last - first + 1)) + first;
+}
 
+var generatedFacts = [];
 function generateRand (){
-
-	
+	// First we need to find all the <div>s with 'showFact' as a
+	// a class and change them back to 'fact'
 	$(".showFact").removeClass().addClass('fact');
 
+	// Then, get all the 'fact' class <div>s
 	var facts = document.getElementsByClassName("fact");
+	var factslength = facts.length;
 
-	facts[generateRandomFact(0,facts.length-1)].className = "showFact";
+	// Use the 'generateRandomNumber' function to create a number
+	// that is between 0 and the total number of 'facts' - 1
+	var randomnumber = generateRandomNumber(0, factslength-1);
+
+	// Check if we've already chosen that number, keep generating
+	// numbers until we get a new one.
+	if(generatedFacts.length > 0){
+		while(generatedFacts.indexOf(randomnumber) != -1){
+			randomnumber = generateRandomNumber(0, factslength-1);
+		}
+	}
+
+	// Set the class of the <div> at the index of the random number
+	// to 'showFact' instead of 'fact'.
+	facts[randomnumber].className = "showFact";
+
+	// Record the randomly generated index number that we used.
+	generatedFacts.push(randomnumber);
+
+	// If we have gone through the whole list of facts, then reset
+	// the list of 'generatedFacts'.
+	if(generatedFacts.length == facts.length){
+		generatedFacts = [randomnumber];
+	}
 }
