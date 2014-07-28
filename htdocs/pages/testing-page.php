fcf7a298005fcee9cf2487c2c31e85deaeb4ef1e
IsaacVH <isaac.vanh@gmail.com>
2014-07-28 15:58:02 -0400

    Setup for testing js, also footer change.

---
+++ htdocs/pages/testing-page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A Page to Test Stuff</title>
+		<?php require("imports.php"); ?>
+		<script type="text/javascript" src="../js/testing.js"></script>
+	</head>
+
+	<body>
+		<div id="home" data-role="page">
+
+			<div id="GameStuff">
+
+			</div>
+
+		</div>
+	</body>
+</html>
