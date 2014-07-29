23ad46f46ed6efe30f5bad155518ea6b5e75dce1
IsaacVH <isaac.vanh@gmail.com>
2014-07-29 11:14:11 -0400

    Testing stuff for game.

--- htdocs/pages/testing-page.php
+++ htdocs/pages/testing-page.php
@@ -2,17 +2,14 @@
 <html>
 	<head>
 		<title>A Page to Test Stuff</title>
-		<?php require("imports.php"); ?>
 		<script type="text/javascript" src="../js/testing.js"></script>
 	</head>
-
-	<body>
-		<div id="home" data-role="page">
-
-			<div id="GameStuff">
-
-			</div>
-
-		</div>
+	<style>
+		canvas {
+			border: 1px solid black;
+		}
+	</style>
+	<body onload="canvasSpaceGame()">
+	    <canvas id="myCanvas" width="846" height="766"></canvas>
 	</body>
 </html>
