323ee1ef8d0257ddb6d4bafecf8e3d0152d09ee2
IsaacVH <isaac.vanh@gmail.com>
2014-08-21 16:47:12 -0400

    Fixed local attractions link

--- htdocs/pages/page-pieces/attraction-list.php
+++ htdocs/pages/page-pieces/attraction-list.php
@@ -7,10 +7,7 @@
 
 <div id="screen" onclick="closeDirections()"></div>
 <div id="popup">
-	<iframe
-	  width="600"
-	  height="450"
-	  frameborder="0" style="border:0"
+	<iframe width="600" height="450" frameborder="0" style="border:0"
 	  src="https://www.google.com/maps/embed/v1/place?key=API_KEY&q=Space+Needle,Seattle+WA">
 	</iframe>
 </div>
