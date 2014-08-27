4bd761c2467ad59af699d3db84bad41d6a40a7a9
retteram <retteramj@gmail.com>
2014-08-27 08:50:54 -0400

    Removed Raleway Links

--- htdocs/pages/page-pieces/imports.php
+++ htdocs/pages/page-pieces/imports.php
@@ -5,8 +5,6 @@
 
 <!-- Fonts -->
 <link rel="stylesheet" href="../libs/font-awesome-4.1.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:400,300">
 
 <!-- jQuery Libraries -->
 <script type="text/javascript" src="../libs/jquery-1.11.1.js"></script>
--- htdocs/pages/page-pieces/imports2.php
+++ htdocs/pages/page-pieces/imports2.php
@@ -1,7 +1,5 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="../css/main_page.css">
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:400,300">
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
 <script type="text/javascript" src="../libs/jquery.touchSwipe.js"></script>
 <script type="text/javascript" src="../js/global.js"></script>
